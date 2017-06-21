4140bd8b92ef5b071a758bfde82ba6d516f2baca
Ca5th <c.ramirezpascual@gmail.com>
2017-06-21 16:51:18 -0400

    Made Recipe creation work

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,29 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(name: params[:name])
+    @recipe = Recipe.new(name: params["recipe"]["name"])
+    recipe_saved = @recipe.save
+    params["steps"].each do |key, value|
+        recipe_text = value["text"]
+        recipe_order = value["order"]
+
+        if(recipe_text != "" and !recipe_order.nil?)
+          @step = Step.new(text: recipe_text, order: recipe_order)
+          @step.recipe = @recipe
+          @step.save
+        end
+    end
+
+    if recipe_saved
+      redirect_to @recipe
+    else
+      @recipe = Recipe.new
+      @steps = []
+      5.times do
+        @steps << Step.new
+      end
+      render 'new'
+    end
 
   end
 
